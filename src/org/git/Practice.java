fe129c26c87104ceb98ef7ea565ca3041aa62e3e
mathan30121996 <mathancietcbe@gmail.com>
2022-05-05 17:04:26 +0530

    yes done

--- src/org/git/Practice.java
+++ src/org/git/Practice.java
@@ -1,9 +1,26 @@
 package org.git;
 
 public class Practice {
+	int a=10;
+	int b=20;
+	int c;
+	private void add() {
+		c=a+b;
+		System.out.println(c);
+
+	}
 	
 	public static void main(String[] args) {
 		System.out.println("mathan");
+		
+		for (int i = 0; i < 5; i++) {
+			System.out.println("yes");
+		}
+		
+		Practice a = new Practice();
+		a.add();
+		
+		
 	}
 	
 	
