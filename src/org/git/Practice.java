42a9720c6fc73c64265e255474af8e53d7dfbabe
mathan30121996 <mathancietcbe@gmail.com>
2022-05-05 18:49:34 +0530

    2nd commit

--- src/org/git/Practice.java
+++ src/org/git/Practice.java
@@ -4,10 +4,11 @@ public class Practice {
 	int a=10;
 	int b=20;
 	int c;
+	String d = "testng";
 	private void add() {
 		c=a+b;
 		System.out.println(c);
-
+		System.out.println("selenium.......................");
 	}
 	
 	public static void main(String[] args) {
@@ -15,6 +16,7 @@ public class Practice {
 		
 		for (int i = 0; i < 5; i++) {
 			System.out.println("yes");
+			System.out.println("mathan >>>>>>>>>>>>>>>>>>>>>>>>");
 		}
 		
 		Practice a = new Practice();
